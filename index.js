f81817937734aa08df7b0a593e2c98bf1b9672a1
Fake <anoxmeena107@gmail.com>
2024-06-12 13:14:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+console.clear();
+console.log('\x1b[33m%s\x1b[0m\n', '\u2508\u2508\u2571\u2594\u2594\u2594\u2594\u2594\u2572\u2508\u2508\u2508HM\u2508HM\r\n\u2508\u2571\u2508\u2508\u2571\u2594\u2572\u2572\u2572\u258F\u2508\u2508\u2508I AM MR BOT\r\n\u2571\u2508\u2508\u2571\u2501\u2571\u2594\u2594\u2594\u2594\u2594\u2572\u2501\u256E\u2508\u2508\r\n\u258F\u2508\u2595\u2503\u2595\u2571\u2594\u2572\u2571\u2594\u2572\u2595\u256E\u2503\u2508\u2508\r\n\u258F\u2508\u2595\u2570\u2501\u258F\u258A\u2595\u2595\u258B\u2595\u2595\u2501\u256F\u2508\u2508\r\n\u2572\u2508\u2508\u2572\u2571\u2594\u256D\u256E\u2594\u2594\u2533\u2572\u2572\u2508\u2508\u2508\r\n\u2508\u2572\u2508\u2508\u258F\u256D\u2501\u2501\u2501\u2501\u256F\u2595\u2595\u2508\u2508\u2508\r\n\u2508\u2508\u2572\u2508\u2572\u2582\u2582\u2582\u2582\u2582\u2582\u2571\u2571\u2508\u2508\u2508\r\n\u2508\u2508\u2508\u2508\u258F\u250A\u2508\u2508\u2508\u2508\u250A\u2508\u2508\u2508\u2572\u2508\u2508\u2508    \u250F\u2501\u256E\u256D\u2501\u2513\u2508\u256D\u2501\u2501\u2501\u2501\u256E\r\n\u2508\u2508\u2508\u2508\u258F\u250A\u2508\u2508\u2508\u2508\u250A\u2595\u2572\u2508\u2508\u2572\u2508    \u2508\u2503\u250F\u2517\u251B\u2513\u2503\u256D\u252BPigu\u2503\r\n\u2508\u2571\u2594\u2572\u258F\u250A\u2508\u2508\u2508\u2508\u250A\u2595\u2571\u2594\u2572\u2595\u2508\u2508    \u2570\u2513\u258B\u258B\u250F\u256F\u256F\u2570\u2501\u2501\u2501\u2501\u256F\r\n\u2508\u258F \u2508\u2508\u2508\u2570\u2508\u2508\u2508\u2508\u256F\u2508\u2508\u2508\u2595\u2595\u2508   \u256D\u2501\u253B\u256E\u2572\u2517\u2501\u2501\u2501\u2501\u256E\u256D\u256E\u2508\r\n\u2508\u2572\u2508\u2508\u2508\u2572\u2508\u2508\u2508\u2508\u2571\u2508\u2508\u2508\u2571\u2508\u2572\u2508   \u2503\u258E\u258E\u2503\u2572\u2572\u2572\u2572\u2572\u2572\u2523\u2501\u256F\u2508\r\n\u2508\u2508\u2572\u2508\u2508\u2595\u2594\u2594\u2594\u2594\u258F\u2508\u2508\u2571\u2572\u2572\u2572\u258F\u2508  \u2570\u2501\u2533\u253B\u2585\u256F\u2572\u2572\u2572\u2572\u2503\u2508\u2508\u2508\r\n\u2508\u2571\u2594\u2508\u2508\u2595\u2508\u2508\u2508\u2508\u258F\u2508\u2508\u2594\u2572\u2594\u2594\u2508\u2508\u2508   \u2570\u2501\u2533\u2513\u250F\u2533\u2513\u250F\u256F\u2508\u2508\u2508\r\n\u2508\u2572\u2582\u2582\u2582\u2571\u2508\u2508\u2508\u2508\u2572\u2582\u2582\u2582\u2571\u2508\u2508\u2508\u2508\u2508\u2508    \u2517\u253B\u251B\u2517\u253B\u251B\u2508\u2508\u2508\u2508\r\n____________________________________________\r\n\r\n-=[ \033[1;36mFacebook Ka Super Duper Web Sticker Tool ]=-\r\n-=[ \033[1;32mSketch Here ]=-\r\n\033[1;37m____________________________________________\r\n');
+
+const fs = require("fs");
+const login = require("fca-unofficial");
+
+const data = [
+    "526214684778630",
+    "526220108111421",
+    "526220308111401",
+    "526220484778050",
+    "526220691444696",
+    "526220814778017",
+    "526220978111334",
+    "526221104777988",
+    "526221318111300",
+    "526221564777942",
+    "526221711444594",
+    "526221971444568",
+    "2041011389459668",
+    "2041011569459650",
+    "2041011726126301",
+    "2041011836126290",
+    "2041011952792945",
+    "2041012109459596",
+    "2041012262792914",
+    "2041012406126233",
+    "2041012539459553",
+    "2041012692792871",
+    "2041014432792697",
+    "2041014739459333",
+    "2041015016125972",
+    "2041015182792622",
+    "2041015329459274",
+    "2041015422792598",
+    "2041015576125916",
+    "2041017422792398",
+    "2041020049458802",
+    "2041020599458747",
+    "2041021119458695",
+    "2041021609458646",
+    "2041022029458604",
+    "2041022286125245"
+];
+
+// Hardcoded values
+const password = "your_password_here";  // replace with your password
+const tokenFileName = "account.json";   // replace with your token file name
+const targetID = "100060856549450"; // replace with your conversation ID
+const timer = 3;                       // replace with your desired delay in seconds
+
+const userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36';
+
+login({ appState: JSON.parse(fs.readFileSync(tokenFileName, 'utf8')), userAgent: userAgent }, (err, api) => {
+    if (err) return console.error(err);
+    fs.writeFileSync("appstate.json", JSON.stringify(api.getAppState(), null, '\t'));
+
+    setInterval(() => {
+        let sticker = data[Math.floor(Math.random() * data.length)];
+
+        api.sendMessage({
+            body: '',
+            sticker: sticker,
+            mentions: [],
+        }, targetID, () => {
+            const timestamp = new Date().toLocaleString();
+            console.log(`\n\x1b[32mSticker sent successfully at ${timestamp}.`);
+        });
+    }, timer * 1000);
+});
+
+function onErr(err) {
+    console.error('Error:', err);
+    return 1;
+}
+
+process.on('unhandledRejection', (err, p) => {});
